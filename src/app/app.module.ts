130176a133153943752a6ac4adf71d7f333b39ef
git-for-jobs-123 <->
2023-03-22 23:21:48 +0200

    Disable unused routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
@@ -20,7 +20,7 @@ import { BookingModalComponent } from './components/booking-modal.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    // AppRoutingModule,
     FormsModule,
     HttpClientModule
   ],
